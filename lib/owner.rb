de94a2a823ee18649324b10ff5e0a7a305dab4c9
Erin Green <erin.y.green@gmail.com>
2018-10-10 15:11:56 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
-  # code goes here
+  
+  attr_reader :species 
+  attr_accessor :pets, :name 
+  
+  @@all = []
+  
+  def initialize(species)
+    @name = name 
+    @species = "human"
+    @@all << self 
+    @pets = {}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+    self.species
+  end
+  
+  def buy_fish
+    
+  end
+  
+  
 end
